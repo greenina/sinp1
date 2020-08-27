d2bbbb8e3f457894cff86970f8895abdca6f1094
ina <igreen0485@gmail.com>
2020-08-27 12:20:19 +0900

    login error user undifined 해결

--- index.js
+++ index.js
@@ -51,21 +51,21 @@ app.post('/api/users/register',(req,res)=>{
 app.post('/api/users/login',(req,res) => {
 
     User.findOne({email: req.body.email},(err,user) => {
-        if(!user) return res.json({loginSuccess: false,message:"email not found"})
+        if(!user) return res.json({loginSuccess: false,message:"email not found"})    
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch){
+                return res.json({loginSuccess:false, message:"wrong password"})
+            }
         });
-    user.comparePassword(req.body.password,(err,isMatch)=>{
-        if(!isMatch){
-            return res.json({loginSuccess:false, message:"wrong password"})
-        }
-    });
-    user.generateToken((err,user)=>{
-        if(err) return res.status(400).send(err);
-        res.cookie("x_auth",user.token)
-            .status(200)
-            .json({
+        user.generateToken((err,user)=>{
+            if(err) return res.status(400).send(err);
+            res.cookie("x_auth",user.token)
+                .status(200)
+                .json({
                 loginSuccess:true
-            })
-    })
+                })
+        })
+    });
 
 })
 
